7a9ce5a43478b388af5c7f7fdb3f1e533655b083
Kimberlee Johnson <kimee.johnson@gmail.com>
2019-09-14 15:29:26 -0700

    Added linear search descriptions and code examples

---
+++ search_algorithms/linear_search/linear_search.md
@@ -0,0 +1,16 @@
+# Linear search
+
+## What is linear search? 
+*Linear search* goes through each item in a list, one by one, until either it finds what it's looking for, or, if it doesn't find what it's searching for, exits out. 
+
+![Linear search](https://www.geeksforgeeks.org/wp-content/uploads/Linear-Search.png)
+_Image via [Geeks for Geeks](https://www.geeksforgeeks.org/linear-search/)_.
+
+## Big O
+Since *linear search* goes through every single item in a list, the amount of time it takes depends directly on the size of the list. Representing n as the number of items in the list, the runtime is *O(n)*. 
+
+## When should I use linear search? 
+If you're working with a _very_ small data set, linear search might make sense. But, it doesn't scale efficiently, so other options make more sense for most real-world applications. 
+
+## In practice 
+Look at the linear_search.py file for examples of this algorithm in practice. 
---
+++ search_algorithms/linear_search/linear_search.py
@@ -0,0 +1,36 @@
+# Linear search takes two arguments: a list of items to search for, and the target. 
+# It returns the index position of the target if found. 
+
+def linear_search(list, target): 
+    
+    # For each item the range of the list, starting at the beginning 
+    for i in range(0, len(list)):
+        # If the target is at the list at position i 
+        if list[i] == target: 
+            # Return the index 
+            return i
+    # Else return None 
+    return None
+
+# Function to verify the output of the function 
+def verify(index): 
+    # If the index exists
+    if index is not None: 
+        # Print where it was found 
+        print("Target found at index: ", index)
+    else: 
+        # Print that the target was not found 
+        print("Target not found in list.")
+
+## Test data sets 
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+strings = ["Truth Hurts", "Juce", "Cuz I Love You", "Tempo"]
+
+result = linear_search(numbers, 12)
+verify(result)
+
+result = linear_search(numbers, 6)
+verify(result)
+
+result = linear_search(strings, "Truth Hurts")
+verify(result)
