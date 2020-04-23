7de2e450c5efe80907d73eb9ee6c7ebfdd697712
Kimberlee Johnson <kimee.johnson@gmail.com>
2020-04-23 11:32:25 -0700

    challenge: added temp pass at solution

---
+++ udemy_javascript_algorithms/sliding_window/findLongestSubstring.js
@@ -0,0 +1,19 @@
+function findLongestSubstring(string){
+    // Cumulative variables 
+    let tempLength = 0; 
+    let maxLength = 0; 
+    
+    // Window/counter variables 
+    let start = 0; 
+    let end = right + 1; 
+    
+    // As long as the end is not the length of the string 
+    while (end <= string.length) {
+        // See if the character to the right of start is unique 
+        // If so, increment the end, make the window bigger 
+        // If not, return the end value, which should be same as number of uniques 
+    }
+    
+    
+  }
+  
