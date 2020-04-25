d0447a8a4d0c238ebfea974575e1003340da5c17
Kimberlee Johnson <kimee.johnson@gmail.com>
2020-04-25 15:50:33 -0700

    Challenge: added comments to first recursive function

--- udemy_javascript_algorithms/recursion/first_recursive_function.js
+++ udemy_javascript_algorithms/recursion/first_recursive_function.js
@@ -1,9 +1,14 @@
 function countDown(num) {
+    // If the number is less than zero 
     if (num <= 0) {
+        // We're done 
         console.log("All done!"); 
         return; 
     }
+    // If not, console.log the number 
     console.log(num); 
+    // Then decrease the number
     num--; 
+    // And call the function again (recursively) on the decrease
     countDown(num); 
 }
