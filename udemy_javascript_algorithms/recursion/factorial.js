43c20ea11bea9a7f7ba6d669d721272035985ad2
Kimberlee Johnson <kimee.johnson@gmail.com>
2020-04-27 12:03:20 -0700

    challenge: added edge case to factorial solution

--- udemy_javascript_algorithms/recursion/factorial.js
+++ udemy_javascript_algorithms/recursion/factorial.js
@@ -10,11 +10,21 @@ function factorial(num) {
     return total; 
 }
 
-// Recursive solution 
+//factorial(1) // 1
+// factorial(2) // 2
+// factorial(4) // 24
+// factorial(7) // 5040
+
 function factorial(num) {
+    // Edge case: if num is 0, the factorial will always be 1
+    if (num === 0) {
+        return 1; 
+    }
+    
     // Base case: if the num is 1, return 1, exit out
     if (num === 1) return 1; 
     // Recursive call
     // If larger than 1, keep multiplying by smaller numbers 
     return num * factorial(num-1); 
 }
+
