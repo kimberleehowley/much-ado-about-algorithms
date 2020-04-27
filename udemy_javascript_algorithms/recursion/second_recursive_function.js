505ee027551e23f9a8ce6c553890328be24ea0f9
Kimberlee Johnson <kimee.johnson@gmail.com>
2020-04-26 19:18:39 -0700

    challenge: added additional comment

--- udemy_javascript_algorithms/recursion/second_recursive_function.js
+++ udemy_javascript_algorithms/recursion/second_recursive_function.js
@@ -1,6 +1,7 @@
 function sumRange (num) {
     // Base case: if num is 1, exit out 
     if (num === 1) return 1; 
-    // Changing input: Return the num plus the num - 1 
+    // But if the num is not 1 
+    // Return it plus the function called again on a number that's one fewer 
     return num + sumRange(num - 1); 
 }
