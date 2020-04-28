3f3277b0ab871bab7c0f296b840c767fbc24a20e
Kimberlee Johnson <kimee.johnson@gmail.com>
2020-04-28 08:11:59 -0700

    challenge: solved productOfArray factorially

--- udemy_javascript_algorithms/recursion/productOfArray.js
+++ udemy_javascript_algorithms/recursion/productOfArray.js
@@ -1,22 +1,13 @@
 // productOfArray([1,2,3]) // 6
 // productOfArray([1,2,3,10]) // 60
-
 function productOfArray(arr) {
-    // Product variable to hold calculation
-    let product = 0; 
-    // Array to hold new array as it gets reduced 
-    let newArr = []; 
-    
-    // Edge case: Array has no numbers? 
-    if (arr.length === 0) {
-        return 0; 
-    }
-    
-    // Base case: array has only one number left 
+    // Edge case: Repeat until 1 item in array, when you return just that one number
     if (arr.length === 1) {
-        return product; 
+        return arr[0]; 
     }
-    product = product * arr[0]; 
-    newArr = arr.slice(1);
-    return productOfArray(newArr); 
+    
+    // Take first item in array 
+    // Multiply by first item in new array without first 
+    return arr[0] * productOfArray(arr.slice(1)); 
+     
 }
