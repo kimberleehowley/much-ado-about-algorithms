1a015bd02ead23ccae9e04641456dbc75c2111ee
Kimberlee Johnson <kimee.johnson@gmail.com>
2020-05-01 19:15:01 -0700

    Added second solution

--- udemy_javascript_algorithms/sliding_window/maxSubarraySum.js
+++ udemy_javascript_algorithms/sliding_window/maxSubarraySum.js
@@ -19,3 +19,37 @@ function maxSubarraySum(array, num){
     }
     return maxSum; 
   }
+
+  function maxSubarraySum(nums, windowSize) {
+    // Edge case: If the sub array num is larger than length of array, exit
+    if (windowSize > nums.length) {
+      return -Infinity;
+    }
+    // Store maxSum variable
+    let maxSum = 0;
+  
+    // Start at the first number of the array
+    // Add the 0-windowSize numbers together in subArray, save as temp sum
+    let tempSum = nums.slice(0, windowSize).reduce((acc, num) => acc + num, 0);
+  
+    console.log(`maxSum:${maxSum} tempSum:${tempSum}`);
+  
+    // Start loop at j=1
+    for (let j = 1; j < nums.length - windowSize + 1; j++) {
+      console.log("in da looop");
+  
+      // Take TempSum, add array[j+1] to temp sum, subtract array[j-1]
+      tempSum = tempSum - nums[j - 1] + nums[j + windowSize - 1];
+      console.log(`tempSum: ${tempSum}`);
+  
+      // Compare TempSum to MaxSum
+      if (tempSum > maxSum) {
+        maxSum = tempSum;
+      }
+    }
+    // Return the MaxSum
+    return maxSum;
+  }
+                              // 1     3  4
+  console.log(maxSubarraySum([1, 2, 3, 7, 8], 3));
+  
