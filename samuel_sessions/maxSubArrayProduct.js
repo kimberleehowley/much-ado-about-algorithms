c131ea5b3b20009494415337f94abfc70c8a6bbe
Kimberlee Johnson <kimee.johnson@gmail.com>
2020-05-03 21:01:49 -0700

    challenge fix: fixed maxProduct to work in all scenarios by changing to equal tempProduct

--- samuel_sessions/maxSubArrayProduct.js
+++ samuel_sessions/maxSubArrayProduct.js
@@ -5,30 +5,32 @@ function maxSubarrayProduct(nums, windowSize) {
     if (windowSize > nums.length) {
       return -Infinity;
     }
-    // Store maxProduct variable
-    let maxProduct = 0;
   
     // Start at the first number in the array
     // Multiply the 0-windowSize numbers together in subArray, save as temp sum
     let tempProduct= nums.slice(0, windowSize).reduce((acc, num) => acc * num, 1);
+
+    // Store maxProduct variable
+    let maxProduct = tempProduct;
   
     console.log(`maxProduct:${maxProduct} tempProduct:${tempProduct}`);
-  
     // Start loop at j=1
     for (let j = 1; j < nums.length - windowSize + 1; j++) {
   
       // Take TempProduct, divide it by the number leaving the window 
       // Multiply the rest by the number remaining in the window 
       tempProduct = (tempProduct / nums[j - 1]) * (nums[j + windowSize - 1]);
-      console.log(`tempProduct: ${tempProduct}`);
   
       // Compare TempSum to MaxSum
       if (tempProduct > maxProduct) {
         maxProduct = tempProduct;
       }
+      console.log(`\ntempProduct: ${tempProduct}`);
+      console.log(`maxProduct:${maxProduct} tempProduct:${tempProduct}`);
+
     }
     // Return the MaxSum
     return maxProduct;
   }
 
-  console.log(maxSubarrayProduct([1, 2, 3, 7, 8], 3));
+  console.log(maxSubarrayProduct([-1, -2, -3, -7, -8], 3));
