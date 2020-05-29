93f2a6148b5213cc3801f83e9d1ae77d34afb68c
Kimberlee Johnson <kimee.johnson@gmail.com>
2020-05-29 09:32:39 -0700

    practice: added notes on linear Big O

--- udemy_javascript_algorithms/searching_algorithms/linear_search.js
+++ udemy_javascript_algorithms/searching_algorithms/linear_search.js
@@ -1,7 +1,8 @@
 // If unsorted, this is how we have to search 
 // indexOf, includes, find, findIndex are all doing linear searches 
+// Linear search is O(n); goes with the length of the array (line)
 
-// Function accepts an array and a value 
+// Function accepts an array and a value // 
 function linearSearch(numbers, target) {
     // Loop through the array and check if the current element is equal to the target
     for (let i = 0; i < numbers.length; i++) {
